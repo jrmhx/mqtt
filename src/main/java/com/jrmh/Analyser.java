992e06e75ccf5fd0a303c4ffb7e4d7dd616a028a
u7439274 <u7439274@anu.edu.au>
2024-05-17 18:05:52 +1000

    v1.0

--- src/main/java/com/jrmh/Analyser.java
+++ src/main/java/com/jrmh/Analyser.java
@@ -8,27 +8,32 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class Analyser {
+    private static final int TIME = 5;
     private static final String BROKER_URL = "tcp://localhost:1883";
     private static final String CLIENT_ID = "analyser";
     private static final String REQUEST_QOS = "request/qos";
     private static final String REQUEST_DELAY = "request/delay";
     private static final String REQUEST_INSTANCE_COUNT = "request/instancecount";
+    private static final String READY_TOPIC = "instruction/ready";
 
     private int[] delays = {0, 1, 2, 4};
     private int[] qoss = {0, 1, 2};
     private int[] instanceCounts = {1, 2, 3, 4, 5};
-    private static final int MESSAGES_PER_SECOND_AT_ZERO_DELAY = 10000; // Estimated number of messages per second at zero delay
 
     public void start() {
         try {
             MqttClient client = new MqttClient(BROKER_URL, CLIENT_ID, new MemoryPersistence());
-            client.connect();
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setMaxInflight(100); // Set the max inflight messages
+
+            client.connect(connOpts);
 
             for (int subQos : qoss) {
                 for (int delay : delays) {
                     for (int pubQos : qoss) {
                         for (int instanceCount : instanceCounts) {
                             publishInstructions(client, pubQos, delay, instanceCount);
+                            sendReadySignal(client);
                             List<String> messages = listenAndCollectData(client, instanceCount, pubQos, delay, subQos);
                             analyzeData(messages, pubQos, delay, instanceCount, subQos);
                         }
@@ -43,36 +48,43 @@ public class Analyser {
     }
 
     private void publishInstructions(MqttClient client, int qos, int delay, int instanceCount) throws MqttException {
-        client.publish(REQUEST_QOS, new MqttMessage(Integer.toString(qos).getBytes()));
-        client.publish(REQUEST_DELAY, new MqttMessage(Integer.toString(delay).getBytes()));
-        client.publish(REQUEST_INSTANCE_COUNT, new MqttMessage(Integer.toString(instanceCount).getBytes()));
+        MqttMessage insCntMsg = new MqttMessage(Integer.toString(instanceCount).getBytes());
+        insCntMsg.setQos(2);
+        client.publish(REQUEST_INSTANCE_COUNT, insCntMsg);
+        MqttMessage qosMsg = new MqttMessage(Integer.toString(qos).getBytes());
+        qosMsg.setQos(2);
+        client.publish(REQUEST_QOS, qosMsg);
+        MqttMessage delayMsg = new MqttMessage(Integer.toString(delay).getBytes());
+        delayMsg.setQos(2);
+        client.publish(REQUEST_DELAY, delayMsg);
     }
+
+    private void sendReadySignal(MqttClient client) throws MqttException {
+        MqttMessage readyMsg = new MqttMessage("ready".getBytes());
+        readyMsg.setQos(2);
+        client.publish(READY_TOPIC, readyMsg);
+    }
+
 
     private List<String> listenAndCollectData(MqttClient client, int instanceCount, int pubQos, int delay, int subQos) throws MqttException, InterruptedException {
         List<String> messages = new ArrayList<>();
         CountDownLatch latch = new CountDownLatch(1);
 
         String topicPath = "counter/#";
-        System.out.println("Listening topic: " + topicPath);
+        System.out.println("instanceCount: " + instanceCount + " pubQos(P2B): " + pubQos + " delay(ms): " + delay + " subQos(B2A): " + subQos);
 
         client.subscribe(topicPath, subQos, (topic, message) -> {
             String payload = new String(message.getPayload());
             messages.add(payload);
         });
 
-        latch.await(60, TimeUnit.SECONDS);
+        latch.await(TIME, TimeUnit.SECONDS);
         client.unsubscribe(topicPath);
         return messages;
     }
 
     private void analyzeData(List<String> messages, int pubQos, int delay, int instanceCount, int subQos) {
         int totalMessages = messages.size();
-        int expectedMessages;
-        if (delay > 0) {
-            expectedMessages = (60 * 1000 / delay) * instanceCount;
-        } else {
-            expectedMessages = MESSAGES_PER_SECOND_AT_ZERO_DELAY * 60 * instanceCount;
-        }
 
         int outOfOrderCount = 0;
         long totalGap = 0;
@@ -89,7 +101,7 @@ public class Analyser {
             previous = current;
         }
 
-        double messageLossRate = ((double) (expectedMessages - totalMessages) / expectedMessages) * 100;
+        // double messageLossRate = ((double) (totalExpectedMessages - totalMessages) / totalExpectedMessages) * 100;
         double outOfOrderRate = ((double) outOfOrderCount / totalMessages) * 100;
         double averageGap = totalMessages > 1 ? (double) totalGap / (totalMessages - 1) : 0;
 
@@ -100,8 +112,8 @@ public class Analyser {
         System.out.println("Instance Count: " + instanceCount);
         System.out.println("--- Results ---");
         System.out.println("Total Messages Received: " + totalMessages);
-        System.out.println("Expected Messages: " + expectedMessages);
-        System.out.println("Message Loss Rate: " + String.format("%.2f", messageLossRate) + "%");
+        // System.out.println("Expected Messages: " + totalExpectedMessages);
+        // System.out.println("Message Loss Rate: " + String.format("%.2f", messageLossRate) + "%");
         System.out.println("Out-of-Order Message Rate: " + String.format("%.2f", outOfOrderRate) + "%");
         System.out.println("Average Inter-Message Gap: " + String.format("%.2f", averageGap) + "ms");
         System.out.println("--------------------------");
--- src/main/java/com/jrmh/Publisher.java
+++ src/main/java/com/jrmh/Publisher.java
@@ -3,13 +3,19 @@ package com.jrmh;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.util.concurrent.CountDownLatch;
 
 public class Publisher {
+    private static final int TIME = 5;
     private static final String BROKER_URL = "tcp://localhost:1883";
     private static final String CLIENT_ID_PREFIX = "pub-";
     private static final String REQUEST_QOS = "request/qos";
     private static final String REQUEST_DELAY = "request/delay";
     private static final String REQUEST_INSTANCE_COUNT = "request/instancecount";
+    private static final String READY_TOPIC = "instruction/ready";
+
+    private static CountDownLatch startLatch = new CountDownLatch(1);
+    private static CountDownLatch doneLatch = new CountDownLatch(5);
 
     private int instance;
     private int qos = 0;
@@ -30,15 +36,20 @@ public class Publisher {
 
             client.connect(connOpts);
 
-            client.subscribe(REQUEST_QOS, this::handleRequest);
-            client.subscribe(REQUEST_DELAY, this::handleRequest);
-            client.subscribe(REQUEST_INSTANCE_COUNT, this::handleRequest);
+            client.subscribe(REQUEST_INSTANCE_COUNT, 2, this::handleRequest);
+            client.subscribe(REQUEST_QOS, 2, this::handleRequest);
+            client.subscribe(REQUEST_DELAY, 2, this::handleRequest);
+            client.subscribe(READY_TOPIC, 2, this::handleReady);
 
             while (true) {
+                // Wait for start signal from all publisher threads
+                startLatch.await();
+
+                long counter = 0;
+
                 if (instance <= activeInstances) {
                     System.out.println("activeInstances: " + activeInstances + " qos: " + qos + " delay: " + delay);
-                    long endTime = System.currentTimeMillis() + 60000; // 60 seconds
-                    long counter = 0;
+                    long endTime = System.currentTimeMillis() + TIME * 1000; // convert s to ms
 
                     while (System.currentTimeMillis() < endTime) {
                         String topic = String.format("counter/%d/%d/%d", instance, qos, delay);
@@ -54,10 +65,20 @@ public class Publisher {
                         counter++;
                         Thread.sleep(delay);
                     }
+
+                }
+
+                // Signal that this thread is done
+                doneLatch.countDown();
+
+                // Reset the latches for the next round
+                if (doneLatch.getCount() == 0) {
+                    startLatch = new CountDownLatch(1);
+                    doneLatch = new CountDownLatch(5);
                 }
 
                 // Sleep briefly before checking for new instructions
-                Thread.sleep(1000);
+                Thread.sleep(TIME);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -79,6 +100,11 @@ public class Publisher {
         }
     }
 
+    private void handleReady(String topic, MqttMessage message) {
+        // Signal all threads to start when receiving the ready message
+        startLatch.countDown();
+    }
+
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             int instance = i;
@@ -86,4 +112,3 @@ public class Publisher {
         }
     }
 }
-
